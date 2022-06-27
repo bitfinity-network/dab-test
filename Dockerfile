71978bf618a3473f7036b81b624cf807d034a0b1
Andy Osei <anyosei36@gmail.com>
2022-06-27 08:57:53 +0000

    add ext canister

---
+++ Dockerfile
@@ -0,0 +1,15 @@
+FROM ghcr.io/infinity-swap/amm-dev:latest
+
+COPY wasm/* ./wasm/
+COPY candid/* ./candid/
+COPY deploy.sh ./
+
+RUN J=$(jq ".canisters += {\"nft_registry\":{\"type\":\"custom\",\"wasm\":\"$WORK_DIR/wasm/nft_registry.wasm\",\"candid\":\"$WORK_DIR/candid/nft_registry.did\"}}" $WORK_DIR/dfx.json) && echo "$J" > $WORK_DIR/dfx.json &&
+    J=$(jq ".canisters += {\"dip721\":{\"type\":\"custom\",\"wasm\":\"$WORK_DIR/wasm/dip721.wasm\",\"candid\":\"$WORK_DIR/candid/dip721.did\"}}" $WORK_DIR/dfx.json) && echo "$J" > $WORK_DIR/dfx.json && \
+    J=$(jq ".canisters += {\"ext\":{\"type\":\"custom\",\"wasm\":\"$WORK_DIR/wasm/ext.wasm\",\"candid\":\"$WORK_DIR/candid/ext.did\"}}" $WORK_DIR/dfx.json) && echo "$J" > $WORK_DIR/dfx.json
+
+RUN (dfx start --background) && ./deploy.sh && (dfx stop)
+
+ENTRYPOINT  [ "/entrypoint.sh" ]
+
+
--- deploy.sh
+++ deploy.sh
@@ -1,19 +1,37 @@
+principal=$(dfx identity get-principal)
+
+# deploy dab nft registry
 dfx deploy nft_registry
 
+# deploy DIP721 nft canister
 dfx deploy dip721 --argument '(opt record { 
     logo = opt "https://fleek.co"; 
     name = opt "dip 721"; 
     symbol = opt "DIP721v2"; 
     custodians = null 
 })'
-
 dip721=$(dfx canister id dip721)
 
+# deploy EXT nft canister
+dfx deploy ext --argument "(principal \"$principal\")"
+ext=$(dfx canister id ext)
+
+# index DIP721 canister in the nft registry
 dfx canister call nft_registry add "( record {
-    name= \"Test NFT\"; 
+    name= \"Dip721 1\"; 
     description= \"Description test\"; 
     thumbnail= \"https://qcg3w-tyaaa-aaaah-qakea-cai.raw.ic0.app/Token/1\"; 
     frontend= opt \"https://fleek.ooo\"; 
     principal_id= principal \"$dip721\"; 
     details= vec { record {\"standard\"; variant { Text= \"DIP721v2\" } } }
-  })"
+})"
+
+# index EXT canister in the nft registry
+dfx canister call nft_registry add "( record {
+    name= \"EXT 1\"; 
+    description= \"EXT NFT\"; 
+    thumbnail= \"https://qcg3w-tyaaa-aaaah-qakea-cai.raw.ic0.app/Token/6\"; 
+    frontend= opt \"https://toniqlabs.com/\"; 
+    principal_id= principal \"$ext\"; 
+    details= vec { record {\"standard\"; variant { Text= \"EXT\" } } }
+})"
